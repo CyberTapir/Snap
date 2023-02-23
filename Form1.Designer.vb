ed439227e485f9ff5842a5288fac98dbe9c21101
CyberTapir <callumtbennett@gmail.com>
2023-02-24 09:43:43 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.picP1Card = New System.Windows.Forms.PictureBox()
+        Me.picP2Card = New System.Windows.Forms.PictureBox()
+        Me.lblP1Card = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.lblP2Card = New System.Windows.Forms.Label()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        CType(Me.picP1Card, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picP2Card, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picP1Card
+        '
+        Me.picP1Card.Location = New System.Drawing.Point(56, 58)
+        Me.picP1Card.Name = "picP1Card"
+        Me.picP1Card.Size = New System.Drawing.Size(106, 144)
+        Me.picP1Card.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picP1Card.TabIndex = 0
+        Me.picP1Card.TabStop = False
+        '
+        'picP2Card
+        '
+        Me.picP2Card.Location = New System.Drawing.Point(206, 58)
+        Me.picP2Card.Name = "picP2Card"
+        Me.picP2Card.Size = New System.Drawing.Size(115, 144)
+        Me.picP2Card.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picP2Card.TabIndex = 1
+        Me.picP2Card.TabStop = False
+        '
+        'lblP1Card
+        '
+        Me.lblP1Card.AutoSize = True
+        Me.lblP1Card.Location = New System.Drawing.Point(85, 229)
+        Me.lblP1Card.Name = "lblP1Card"
+        Me.lblP1Card.Size = New System.Drawing.Size(41, 15)
+        Me.lblP1Card.TabIndex = 2
+        Me.lblP1Card.Text = "Label1"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(250, 229)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(0, 15)
+        Me.LinkLabel1.TabIndex = 3
+        '
+        'lblP2Card
+        '
+        Me.lblP2Card.AutoSize = True
+        Me.lblP2Card.Location = New System.Drawing.Point(240, 229)
+        Me.lblP2Card.Name = "lblP2Card"
+        Me.lblP2Card.Size = New System.Drawing.Size(41, 15)
+        Me.lblP2Card.TabIndex = 4
+        Me.lblP2Card.Text = "Label2"
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(147, 285)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlay.TabIndex = 5
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(370, 353)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.lblP2Card)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.lblP1Card)
+        Me.Controls.Add(Me.picP2Card)
+        Me.Controls.Add(Me.picP1Card)
+        Me.Name = "Form1"
+        Me.Text = "Snap"
+        CType(Me.picP1Card, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picP2Card, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picP1Card As PictureBox
+    Friend WithEvents picP2Card As PictureBox
+    Friend WithEvents lblP1Card As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents lblP2Card As Label
+    Friend WithEvents btnPlay As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Windows.Forms.VisualStyles
+Public Class Form1
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        Dim p1card As Integer
+        Dim p2card As Integer
+        p1card = Int(52 * Rnd()) + 1
+        Do
+            p2card = Int(52 * Rnd()) + 1
+        Loop untilk p1card <> p2card 
+        lblP1Card.Text = p1card
+        lblP2Card.Text = p2card
+        picP1Card.ImageLocation = "card graphics\" & getCardName(p1card) & ".png"
+        picP2Card.ImageLocation = "card graphics\" & getCardName(p2card) & ".png"
+        If snapDetect(p1card, p2card) = True Then
+            MsgBox("Snap!")
+        End If
+    End Sub
+    Private Function getCardName(cardNum) As String
+        Dim result As String
+        Select Case cardNum
+            Case 1 : result = "ad"
+            Case 2 : result = "2d"
+            Case 3 : result = "3d"
+            Case 4 : result = "4d"
+            Case 5 : result = "5d"
+            Case 6 : result = "6d"
+            Case 7 : result = "7d"
+            Case 8 : result = "8d"
+            Case 9 : result = "9d"
+            Case 10 : result = "td"
+            Case 11 : result = "jd"
+            Case 12 : result = "qd"
+            Case 13 : result = "kd"
+            Case 14 : result = "ah"
+            Case 15 : result = "2h"
+            Case 16 : result = "3h"
+            Case 17 : result = "4h"
+            Case 18 : result = "5h"
+            Case 19 : result = "6h"
+            Case 20 : result = "7h"
+            Case 21 : result = "8h"
+            Case 22 : result = "9h"
+            Case 23 : result = "th"
+            Case 24 : result = "jh"
+            Case 25 : result = "qh"
+            Case 26 : result = "kh"
+            Case 27 : result = "ac"
+            Case 28 : result = "2c"
+            Case 29 : result = "3c"
+            Case 30 : result = "4c"
+            Case 31 : result = "5c"
+            Case 32 : result = "6c"
+            Case 33 : result = "7c"
+            Case 34 : result = "8c"
+            Case 35 : result = "9c"
+            Case 36 : result = "tc"
+            Case 37 : result = "jc"
+            Case 38 : result = "qc"
+            Case 39 : result = "kc"
+            Case 40 : result = "as"
+            Case 41 : result = "2s"
+            Case 42 : result = "3s"
+            Case 43 : result = "4s"
+            Case 44 : result = "5s"
+            Case 45 : result = "6s"
+            Case 46 : result = "7s"
+            Case 47 : result = "8s"
+            Case 48 : result = "9s"
+            Case 49 : result = "ts"
+            Case 50 : result = "js"
+            Case 51 : result = "qs"
+            Case 52 : result = "ks"
+        End Select
+        Return result
+    End Function
+
+    Private Function snapDetect(p1card As Integer, p2card As Integer) As Boolean
+        Dim result As Boolean
+        If p1card Mod 13 = p2card Mod 13 Then
+            result = True
+        Else
+            result = False
+        End If
+        Return result
+    End Function
+End Class
